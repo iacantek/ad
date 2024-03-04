e835658db7d91c3b7780b1931062ffe8e4b11f86
iacantek <alan.cantekin@hslu.ch>
2024-03-04 15:44:14 +0100

    add: sw01
    - exercise 4
    - exercise 5
    - exercise 6
    - exercise 7

---
+++ src/main/java/ch/hslu/sw01/Exercise4.java
@@ -0,0 +1,64 @@
+package ch.hslu.sw01;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercise4 {
+    private static final List<Long> fibonacciCache = new ArrayList<Long>(200);
+
+    private static final Logger LOG =
+            LoggerFactory.getLogger(Exercise4.class);
+
+    public static void main(String[] args) {
+    }
+
+    public static long fiboRec1(int n) {
+        // Rekursionsbasis
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        // Rekursionsvorschrift
+        return fiboRec1(n - 1) + fiboRec1(n - 2);
+    }
+
+    public static long fiboRec2(int n) {
+        // Rekursionsbasis
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        if (n >= fibonacciCache.size()) {
+            for (int i = 0; i <= n; i++) {
+                fibonacciCache.add(null);
+            }
+        }
+
+        if(n >= fibonacciCache.size() || fibonacciCache.get(n) == null){
+            // Rekursionsvorschrift
+            var fibN = fiboRec1(n - 1) + fiboRec1(n - 2);
+            fibonacciCache.set(n, fibN);
+            return fibN;
+        }else{
+            return fibonacciCache.get(n);
+        }
+
+    }
+
+    public static long fiboIter(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        var minusTwo = 0;
+        var minusOne = 1;
+
+        for (int i = 2; i < n; i++) {
+            var current = minusTwo + minusOne;
+            minusTwo = minusOne;
+            minusOne = current;
+        }
+
+        return minusOne + minusTwo;
+    }
+}
---
+++ src/main/java/ch/hslu/sw01/Exercise7.java
@@ -0,0 +1,75 @@
+package ch.hslu.sw01;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Exercise7 {
+    private static final Logger LOG =
+            LoggerFactory.getLogger(Exercise7.class);
+
+    public static void main(String[] args) {
+        LOG.info("Symmetrische 1-Stellige Zahlen: " + amountOfSymmetricNumbers(1));
+        LOG.info("Symmetrische 2-Stellige Zahlen: " + amountOfSymmetricNumbers(2));
+        LOG.info("Symmetrische 3-Stellige Zahlen: " + amountOfSymmetricNumbers(3));
+        LOG.info("Symmetrische 4-Stellige Zahlen: " + amountOfSymmetricNumbers(4));
+        LOG.info("Symmetrische 5-Stellige Zahlen: " + amountOfSymmetricNumbers(5));
+        LOG.info("Symmetrische 6-Stellige Zahlen: " + amountOfSymmetricNumbers(6));
+        LOG.info("Symmetrische 7-Stellige Zahlen: " + amountOfSymmetricNumbers(7));
+        LOG.info("Symmetrische 8-Stellige Zahlen: " + amountOfSymmetricNumbers(8));
+//        LOG.info("Symmetrische 9-Stellige Zahlen: " + numberOfSymmetricNumbers(9));
+
+    }
+
+    public static int amountOfSymmetricNumbers(int amountOfDigits) {
+        var lower = (int) Math.pow(10, amountOfDigits - 1);
+        var upper = lower * 10;
+
+        if (lower == 1) return 10;
+
+        var numberOfSymmetricNumbers = 0;
+        for (int i = lower; i < upper; i++) {
+            if (isSymmetric(i))
+                numberOfSymmetricNumbers++;
+        }
+
+        return numberOfSymmetricNumbers;
+    }
+
+    public static int getNextSymmetricNumber(int number) {
+        while (!isSymmetric(number)) {
+            number++;
+        }
+
+        return number;
+    }
+
+    /**
+     * Beschreibung des Algorithmus:
+     * Falls Zahl eine Länge von 1 hat, ist die Zahl immer symmetrisch.
+     * Falls nicht, wird das erste Zeichen mit dem letzten Zeichen verglichen. Wenn diese nicht übereinstimmen,
+     * ist die Zahl nicht symmetrisch.
+     * Weiter wird der Index vom Anfang der Zahl inkrementiert und der Index vom Ende der Zahl dekrementiert.
+     * Die Zeichen an den neuen Indexen werden verglichen.
+     * Das geht so weiter bis die Indexe sich in der Mitte getroffen haben.
+     *
+     * @param number Nummer, welche auf Symmetrie geprüft werden soll
+     * @return True, wenn number symmetrisch ist
+     */
+    public static boolean isSymmetric(Integer number) {
+        var numberString = number.toString();
+
+        if (numberString.length() == 1) return true;
+
+        var currentIndex = 0;
+
+        var maxIndex = numberString.length() - 1;
+        while ((maxIndex / 2) >= currentIndex) {
+            if (numberString.charAt(currentIndex) != numberString.charAt(maxIndex - currentIndex))
+                return false;
+
+            currentIndex++;
+        }
+
+        return true;
+    }
+}
